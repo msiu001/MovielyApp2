a88775e9405480540da87cb873048705b7ed1793
unknown <msiu001@fiu.edu>
2018-08-15 23:08:19 -0400

    adding a label to the input boxes with the focus to the respective textboxes

--- Moviely/Moviely/Models/Customer.cs
+++ Moviely/Moviely/Models/Customer.cs
@@ -9,12 +9,15 @@ namespace Moviely.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name= "Date of Birthday")]
         public DateTime? Birthdate { get; set; }
     }
 }
