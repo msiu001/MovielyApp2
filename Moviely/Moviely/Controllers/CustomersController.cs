132a222d04076a3e5d3efb3e8761b211c5f9dbfb
unknown <msiu001@fiu.edu>
2018-08-20 23:16:17 -0400

    adding a dropdownlist of membershiptype and create a ViewModel to encapsulate all data required by this view

--- Moviely/Moviely/Controllers/CustomersController.cs
+++ Moviely/Moviely/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moviely.ViewModels;
 
 namespace Moviely.Controllers
 {
@@ -26,7 +27,12 @@ namespace Moviely.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
         }
 
 
---
+++ Moviely/Moviely/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Moviely.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Moviely.ViewModels
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
